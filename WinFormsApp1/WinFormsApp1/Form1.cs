37dbfa2a56bf31682658f35fc80b87c564226963
llajtxp <162139760+llajtxp@users.noreply.github.com>
2024-11-06 14:13:12 +0100

    Add files via upload

---
+++ WinFormsApp1/WinFormsApp1/Form1.cs
@@ -0,0 +1,25 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
